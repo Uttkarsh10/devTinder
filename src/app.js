782067599612104b63f6f11202e560ef8c89fc6b
Uttkarsh <uttkarshsaini10@gmail.com>
2024-11-01 00:21:30 +0530

    Added Schema level validation for email

--- src/app.js
+++ src/app.js
@@ -6,19 +6,72 @@
  const {adminAuth} = require("./middlewares/auth")
  const User = require("./models/user");
 
+
+app.use(express.json());
+
 app.post("/signup", async (req,res) => {
 
-   const user = new User({
-      firstName : "Tary",
-      lastName : "Saini",
-      email : "Taru@gmail.com",
-      password : "Taru@123",
-   });
+   const user = new User(req.body);
+   // const user = new User({
+   //    firstName : "Tary",
+   //    lastName : "Saini",
+   //    email : "Taru@gmail.com",
+   //    password : "Taru@123",
+   // });
 
    try{
       await user.save();
       res.send("User Added Successfully");
    } catch(err){
+      res.status(400).send("Error Sending the user data" + err.message);
+   }
+})
+
+app.get("/user", async (req,res) => {
+
+   const email = req.body.email;
+   
+   try{
+      // console.log(email);
+      // const Users = await User.find({email:email});
+      const Users = await User.find({});
+      res.send(Users);
+   }
+
+   catch(err){
+      res.status(400).send("Error Sending the user data" + err.message);
+   }
+});
+
+app.patch("/user", async (req,res) => {
+   const id = req.body.userId;
+   // const name = req.body.firstname;
+
+   try{
+      const updatedUser = await User.findOneAndUpdate({_id:id}, req.body)
+      // const updatedUser = await User.findOneAndUpdate({firstName:name}, {firstName:"Taru"})
+      console.log(updatedUser);
+      res.send("User Updated");
+   }
+   catch(err){
+      res.status(400).send("Error Sending the user data" + err.message);
+   }
+});
+
+app.delete("/user", async(req,res) => {
+
+   // const name = req.body.firstname;
+   const id = req.body.id;
+   
+   try{
+      // console.log(name);
+      // const deletedUser = await User.deleteOne({firstName:name});
+      const deletedUser = await User.findByIdAndDelete(id);
+      
+      res.send(deletedUser);
+   }
+
+   catch(err){
       res.status(400).send("Error Sending the user data" + err.message);
    }
 })
--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,15 @@
 const { Mongoose, default: mongoose } = require("mongoose")
+const {validator} = require("validator")
 
 const userSchema = new mongoose.Schema({
     firstName : String,
     lastName : String,
-    email : String,
+    email : {
+        type: String,
+        validate(value){
+            if(!validator.isEmail(value)) throw new Error("Invalid Emaild Id : " + value);
+        }
+    },
     password : String,
     gender : String,
     age : Number
