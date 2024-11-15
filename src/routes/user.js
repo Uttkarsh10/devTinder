b024c1e7a4a61aa8a7dfd769e6acf91da7dd1fea
Uttkarsh <uttkarshsaini10@gmail.com>
2024-11-16 00:17:27 +0530

    Backend Implemented

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 userRouter.get("/user/requests/recieved", userAuth, async(req,res) => {
@@ -15,7 +16,6 @@ userRouter.get("/user/requests/recieved", userAuth, async(req,res) => {
     } catch (error) {
         res.status(400).send("Error : " + error.message)
     }
-
 
 })
 
@@ -43,7 +43,46 @@ userRouter.get("/user/connections", userAuth, async(req,res) => {
         res.status(400).send("Error : " + error.message)
     }
 
+})
 
+
+userRouter.get("/feed", userAuth, async(req,res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+
+        const skip = (page-1)*limit;
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [
+                {recieverUserID : loggedInUser._id},
+                {senderUserID : loggedInUser._id}  
+            ]
+        })
+
+        //We only want those profile to whom we haven't sent, recieved the connection request
+        const hideProfiles = new Set();
+        connectionRequest.map((data) => {
+            hideProfiles.add(data.recieverUserID.toString());
+            hideProfiles.add(data.senderUserID.toString());
+        });
+
+        //Now for feed, we want those profiles who are not present in the hiddenProfiles and also our own profile should not be there, Hence the 2nd check.
+        const feedProfiles = await User.find({
+            $and : [
+                {_id : {$nin : Array.from(hideProfiles)}},
+                {_id : {$ne : loggedInUser._id}},
+            ]
+        }).select(["firstName", "lastName", "about", "skills"]).skip(skip).limit(limit);
+
+        res.send(feedProfiles);
+
+    } catch (error) {
+        res.status(400).json({message : error.message});    
+    }
 })
 
 module.exports = userRouter;
